058ee9074f515498bb02b9b2a4c06a8f17704da6
Kevin Nayar <kevin.nayar@gmail.com>
2023-05-15 14:15:15 -0500

    cleanup up roles decorator

--- src/common/decorators/roles.decorator.ts
+++ src/common/decorators/roles.decorator.ts
@@ -1,18 +1,27 @@
 import { createParamDecorator, ExecutionContext } from '@nestjs/common';
-import { UserEntity, UserRoles } from '../types/user.types';
+import { UserEntity } from '../types/user.types';
+
+function getRoles(
+  ctx: ExecutionContext,
+  scope: 'realm' | 'resource',
+): string[] {
+  const request = ctx.switchToHttp().getRequest();
+  const user = request.user as UserEntity;
+
+  switch (scope) {
+    case 'realm':
+      return user?.roles?.realm || [];
+    case 'resource':
+      return user?.roles?.resource || [];
+    default:
+      return [];
+  }
+}
 
 export const RolesForRealm = createParamDecorator(
-  (_data: unknown, ctx: ExecutionContext) => {
-    const request = ctx.switchToHttp().getRequest() as { user: UserEntity };
-    const { roles }: { roles: UserRoles } = request.user;
-    return roles?.realm || [];
-  },
+  (_d: unknown, ctx: ExecutionContext) => getRoles(ctx, 'realm'),
 );
 
 export const RolesForResource = createParamDecorator(
-  (_data: unknown, ctx: ExecutionContext) => {
-    const request = ctx.switchToHttp().getRequest() as { user: UserEntity };
-    const { roles }: { roles: UserRoles } = request.user;
-    return roles?.resource || [];
-  },
+  (_d: unknown, ctx: ExecutionContext) => getRoles(ctx, 'resource'),
 );
